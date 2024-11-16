7f218f282fd3ed50278bfa21aa20b8799dd385c6
Jack Applegate <japplegate2321@gmail.com>
2024-11-16 22:58:03 +0000

    Add checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,7 +5,7 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone number',
+        fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country', 
                   'county',)
@@ -33,7 +33,7 @@ class OrderForm(forms.ModelForm):
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             else:
-                placeholder - placeholders[field]
+                placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
