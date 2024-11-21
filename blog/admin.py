032be5498b04dec4305dc40466d58c4361004c85
Jack Applegate <japplegate2321@gmail.com>
2024-11-21 19:25:35 +0000

    Add card to blog template and register model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'main_title',
+        'sub_title',
+        'post_content',
+    )
+
+admin.site.register(BlogPost, BlogPostAdmin)
