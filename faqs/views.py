53c7bdf77439a7ef967bc05a5c4ce333085423cf
Jack Applegate <japplegate2321@gmail.com>
2024-11-21 11:43:59 +0000

    Initialise FAQs app, urls, view, and model

---
+++ faqs/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Faq
+# Create your views here.
+
+def all_faqs(request):
+    """ A view to show all frequently asked questions """
+
+    faqs = Faq.objects.all()
+    context = {
+        'faqs': faqs,
+    }
+
+    return render(request, 'faqs/faqs.html', context)
+
+
