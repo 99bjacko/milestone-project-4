fbcf8a1d696955b6e91ac1be336a4504060981ac
Jack Applegate <japplegate2321@gmail.com>
2024-11-22 09:02:57 +0000

    Set up app ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn revitalise.wsgi:application
--- revitalise/settings.py
+++ revitalise/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-1-yh7b597(6dw-$3i_ds*ftqxe!fgan*ppood+b%6om%=**hzf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-99bjacko-milestoneproje-0h7iv8oj13m.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-99bjacko-milestoneproje-0h7iv8oj13m.ws.codeinstitute-ide.net', 'revitalise-project-0a7838062db9.herokuapp.com']
 
 
 # Application definition
@@ -121,12 +122,21 @@ WSGI_APPLICATION = 'revitalise.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        default: dj_default_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+# DATABASES = {
+#     'default': dj_database_url.parse('postgresql://u4tpfkiftia:BzbkiJ1ZRuRH@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/bunt_bunch_match_159488')
+# }
 
 
 # Password validation
